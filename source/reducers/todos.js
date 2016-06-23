9e29a53a55441c0a2ddab9b12a651e3fb447d53b
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2016-06-23 09:24:35 -0400

    Cleanup actions & reducers

--- source/reducers/todos.js
+++ source/reducers/todos.js
@@ -2,7 +2,7 @@ import { FETCH_TODOS_REQUEST, FETCH_TODOS_SUCCESS } from '../actions'
 
 const initialState = {
 	isLoading: false,
-	todos: []
+	todos: {}
 };
 
 function todos(state = initialState, action) {
@@ -10,7 +10,7 @@ function todos(state = initialState, action) {
 		case FETCH_TODOS_REQUEST:
 			return { ...state, isLoading: true };
 		case FETCH_TODOS_SUCCESS:
-			return { ...state, isLoading: false, todos: payload.todos };
+			return { ...state, isLoading: false, todos: action.todos };
 		default:
 			return state;
 	}
