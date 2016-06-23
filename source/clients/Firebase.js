1d54d5f37436fccd80a86c04f55cb3259516469d
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2016-06-23 16:45:46 -0400

    Remove comments & minor cleanup

--- source/clients/Firebase.js
+++ source/clients/Firebase.js
@@ -5,8 +5,6 @@ firebase.initializeApp(firebaseConfig);
 
 class Firebase {
 
-	static read = path => firebase.database().ref(path);
-
 	static create = (path, value) => {
 		return new Promise((resolve, reject) => {
 			firebase
@@ -15,6 +13,8 @@ class Firebase {
 				.push(value, error => error ? reject(error) : resolve());
 		});
 	}
+
+	static read = path => firebase.database().ref(path);
 
 	static update = (path, value) => {
 		return new Promise((resolve, reject) => {
@@ -37,23 +37,3 @@ class Firebase {
 }
 
 export default Firebase;
-
-// export const update = (todoId, data) => database.ref(`todos/${todoId}`)
-// 	.set(data)
-// 	.then(() => console.log('Update succeeded.'))
-// 	.catch(error => console.log(`Update failed: ${error.message}`));
-//
-// export const remove = todoId => database.ref(`todos/${todoId}`)
-// 	.remove()
-// 	.then(() => console.log('Delete succeeded.'))
-// 	.catch(error => console.log(`Delete failed: ${error.message}`));
-//
-
-
-// TODO: need to convert this into proper clients
-// https://github.com/douglascorrea/react-hot-redux-firebase-starter/blob/master/src/api/firebase.js
-// https://firebase.google.com/docs/database/web/save-data
-// import { createTodo, getTodos, deleteTodo } from '../clients/Todo';
-// const todoId = createTodo({ title: 'todo' });
-// getTodos();
-// deleteTodo(todoId);
