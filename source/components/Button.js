a04e1ab46ff1639d38969dc84efc2cf1ac642bdf
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2016-06-24 15:23:00 -0400

    Remove unneeded component

--- source/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import classNames from '../utils/classNames';
-
-
-class Button extends Component {
-	render = () => <button className={classNames('btn', 'btn-primary')}>{this.props.children}</button>;
-}
-
-export default Button
